62642e5821de7c9ed09d54b6b56c687f8452c447
keith horton <keith@kali.home>
2021-02-01 07:10:02 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    size_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer(){
+    InputBuffer* input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+int main(int argc, char* argv[]){
+
+    return 0;
+}
